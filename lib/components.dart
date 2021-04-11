8836025332a32af9dca5e52e879fab1cca4db664
Harshit Srivastava <67497473+harshitsrivastava1608@users.noreply.github.com>
2021-04-11 14:33:54 +0530

    Initial Working

---
+++ lib/components.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tictactoe_flutter/main.dart';
+class AlertMessage extends StatefulWidget {
+  @override
+  _AlertMessageState createState() => _AlertMessageState();
+}
+
+class _AlertMessageState extends State<AlertMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextFieldAlertDialog(),
+    );
+  }
+}
+class TextFieldAlertDialog extends StatefulWidget {
+  @override
+  _TextFieldAlertDialogState createState() => _TextFieldAlertDialogState();
+}
+
+class _TextFieldAlertDialogState extends State<TextFieldAlertDialog> {
+  TextEditingController _textFieldController1 = TextEditingController();TextEditingController _textFieldController2 = TextEditingController();
+String player1="",player2="";
+initial1(){
+  setState(() {
+    player1=_textFieldController1.text.toString();
+  });
+}initial2(){
+    setState(() {
+      player2=_textFieldController2.text.toString();
+    });
+  }
+  _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Scaffold(
+
+            body: AlertDialog(
+
+              title: Text('On Your Marks!'),
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(player1+"*"+player2),
+                  TextField(
+                    controller: _textFieldController1,
+                    decoration: InputDecoration(hintText: "Name Player1",),
+                    onChanged: (value) => player1 = value,onSubmitted: (value) => player1 = value,
+                    onEditingComplete: (){
+                      setState(() {
+                        initial1();
+                        player1=_textFieldController1.text.toString();
+                      });
+                    },
+                  ),
+                  TextField(
+                    controller: _textFieldController2,
+                    decoration: InputDecoration(hintText: "Name Player2"),
+                    onChanged: (value) => player2 = value,onSubmitted: (value) => player2 = value,
+                    onEditingComplete: (){
+                      setState(() {
+                        initial2();
+                        player2=_textFieldController2.text.toString();
+                      });
+                    },
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+
+                new ElevatedButton(
+
+                  child: new Text('Start Game'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size(300, 50))),
+                  onPressed: () {
+                    setState(() {
+                      initial1();initial2();
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>MyGame(player1,player2)));
+                    });
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+
+          Container(alignment:Alignment.topCenter,child: Text("TIC-TAC-TOE",style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,color: Colors.amber),)),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+              style: ButtonStyle(backgroundColor:MaterialStateProperty.all(Colors.yellow),fixedSize: MaterialStateProperty.all(Size(300, 100)) ),
+              child: Text(
+                'Getting Started',
+                  style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,color: Colors.white,),),
+
+              onPressed: () => _displayDialog(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_flutter/components.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AlertMessage(),//MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyGame extends StatelessWidget {
+  String p="",q="";
+  MyGame(String p,String q){
+    this.p=p;this.q=q;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:p+q,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(p,q),//MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // MyHomePage({Key key, this.title}) : super(key: key);
+  // final String title;
+  String p1="",p2="";
+  MyHomePage(String p1,String p2){
+    this.p1=p1;this.p2=p2;
+  }
+  @override
+  _MyHomePageState createState() => _MyHomePageState(p1,p2);
+}
+class _MyHomePageState extends State<MyHomePage> {
+  String player1="",player2="";
+  _MyHomePageState(String p1,String p2){
+    player1=p1;
+    player2=p2;
+  }
+  int _turn=0;String text="";List _list=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];String result="Winner";
+  isPressed(int n){
+    ++_turn;
+    if(_turn%2==0) {
+      text = "X";
+      _list[n]=1;
+    }
+    else {
+      text = "O";
+      _list[n]=0;
+    }
+    if(_turn>4)
+      result=result+" "+checkWinner();
+  }
+  checkWinner(){
+    if((_list[1]==_list[2])&&(_list[2]==_list[3])) {
+      if(_list[1]==0)
+        return player1;
+      if(_list[1]==1)
+        return player2;
+    }
+    if((_list[1]==_list[4])&&(_list[4]==_list[7])) {
+      if(_list[1]==0)
+        return player1;
+      if(_list[1]==1)
+        return player2;
+    }
+    if((_list[2]==_list[5])&&(_list[5]==_list[8])) {
+      if(_list[2]==0)
+        return player1;
+      if(_list[2]==1)
+        return player2;
+    }
+    if((_list[4]==_list[5])&&(_list[5]==_list[6])) {
+      if(_list[4]==0)
+        return player1;
+      if(_list[4]==1)
+        return player2;
+    }
+    if((_list[7]==_list[8])&&(_list[8]==_list[9])) {
+      if(_list[7]==0)
+        return player1;
+      if(_list[7]==1)
+        return player2;
+    }
+    if((_list[3]==_list[6])&&(_list[6]==_list[9])) {
+      if(_list[3]==0)
+        return player1;
+      if(_list[3]==1)
+        return player2;
+    }
+    if((_list[3]==_list[5])&&(_list[5]==_list[7])) {
+      if(_list[3]==0)
+        return player1;
+      if(_list[3]==1)
+        return player2;
+    }
+    if((_list[1]==_list[5])&&(_list[5]==_list[9])) {
+      if(_list[1]==0)
+        return player1;
+      if(_list[1]==1)
+        return player2;
+    }
+    else
+      return "";
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //appBar: AppBar(title: Text(p1+p2),),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 30, 30, 20),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(1);
+                            });
+                          },
+                            child: Text(_list[1].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed:  (){
+                            setState(() {
+                              isPressed(2);
+                            });
+                          }, child: Text(_list[2].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(3);
+                            });
+                          }, child: Text(_list[3].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(4);
+                            });
+                          }, child: Text(_list[4].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed:(){
+                            setState(() {
+                              isPressed(5);
+                            });
+                          }, child: Text(_list[5].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(6);
+                            });
+                          }, child: Text(_list[6].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(7);
+                            });
+                          }, child: Text(_list[7].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(8);
+                            });
+                          }, child: Text(_list[8].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              isPressed(9);
+                            });
+                          }, child: Text(_list[9].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent),fixedSize: MaterialStateProperty.all(Size(100, 100))),),
+                        ],
+                      )
+
+                    ],
+                  ),
+                ),
+              ),
+
+              Text(result),
+              //Text(p1+p2)
+            ],
+          ),
+        )
+    );
+  }
+}
